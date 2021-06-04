072ad6cc6add2cb32fbf9c9353b93b323b096483
Shoichi-Ikeda <shoichann@gmail.com>
2021-06-05 08:57:42 +0900

    Finish Java Chapter04

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int NUMBER = 777;
+		String name = "池田翔一";
+		System.out.println(name);
+
+		name = "得意淡然失意泰然";
+		System.out.println(name);
+		System.out.println(NUMBER);
+	}
+}
---
+++ JavaWcp/src/variable01.java
@@ -0,0 +1,23 @@
+
+public class variable01 {
+	public static void main(String[] args) {
+		//int型の変数num1を宣言
+		int num1;
+		//String型の変数str1を宣言
+		String str1;
+		//num1を初期化
+		num1 = 10;
+		//str1を初期化
+		str1 = "Hello World!!";
+		System.out.println(num1);
+		System.out.println(str1);
+
+		//num1に20を再代入
+		num1 = 20;
+		System.out.println(num1);
+
+		//int型の変数num2を宣言と同時に初期化
+		int num2 = 30;
+		System.out.println(num2);
+	}
+}
