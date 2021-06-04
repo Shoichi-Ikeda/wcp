4a36a7d22390ed1a4ca84a9e1cef2d37eb90f901
Shoichi-Ikeda <shoichann@gmail.com>
2021-06-05 08:05:45 +0900

    Finish Java Chapter03

--- JavaWcp/src/Ready02.java
+++ JavaWcp/src/Ready02.java
@@ -1,5 +1,5 @@
 public class Ready02 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
