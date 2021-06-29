e29008bfc55127859a01900386467a0b51ff4a9f
Shoichi-Ikeda <shoichann@gmail.com>
2021-06-29 14:56:12 +0900

    Add Java Chapter06

---
+++ JavaWcp/src/Mathmatic06.java
@@ -0,0 +1,18 @@
+//文字列の連結
+public class Mathmatic06 {
+	public static void main(String[] args) {
+		String a = "100";
+		String b = "200";
+		String c;
+		int d = 300;
+		int e = 400;
+		int f;
+		
+		c = a + b;
+		System.out.println(c);
+		c = "Shoichi" + "Ikeda";
+		System.out.println(c);
+		f = d + e;
+		System.out.println(f);
+	}
+}
