8d9434d7a8af9b2cd99bc5a2f3acb2242f5ff73d
Shoichi-Ikeda <shoichann@gmail.com>
2021-06-29 14:45:11 +0900

    Add Java Chapter06

---
+++ JavaWcp/src/Mathmatic04.java
@@ -0,0 +1,20 @@
+//関係(比較)演算子
+public class Mathmatic04 {
+	public static void main(String[] Args) {
+		int a = 5;
+		boolean c;
+		
+		c = a < 5;
+		System.out.println(c);
+		c = a <= 5;
+		System.out.println(c);
+		c = a > 4;
+		System.out.println(c);
+		c = a >= 6;
+		System.out.println(c);
+		c = a == 5;
+		System.out.println(c);
+		c = a != 5;
+		System.out.println(c);
+	}
+}
