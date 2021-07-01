b5a71897f075f30c3f435d02a2ab878ff20eb6b9
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-01 15:59:16 +0900

    Add Age01.java

---
+++ JavaWcp/src/Age01.java
@@ -0,0 +1,13 @@
+/* サンプルプログラム Age01
+ * 開発:池田翔一 作成日:2021年
+ */
+//ソースファイル名は「class名.java」
+//class名はアルファベットの大文字
+public class Age01 {
+	//ここからmainメソッド
+	public static void main(String[] args) {
+		int age; //年齢を入れる箱
+		age = 31;
+		System.out.println("私は" + age + "歳です。");
+	}
+}
