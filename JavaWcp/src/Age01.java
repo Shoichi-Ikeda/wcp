35f6a293f9577dc338fa821caccc1c3b055069e1
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-03 07:33:14 +0900

    Add Age02.java

--- JavaWcp/src/Age01.java
+++ JavaWcp/src/Age01.java
@@ -7,7 +7,7 @@ public class Age01 {
 	//ここからmainメソッド
 	public static void main(String[] args) {
 		int age; //年齢を入れる箱
-		age = 31;
-		System.out.println("私は" + age + "歳です。");
+		age = 31; //箱に数字の「31」をいれる
+		System.out.println("私は" + age + "歳です。"); //箱の中身を表示する
 	}
 }
---
+++ JavaWcp/src/Age02.java
@@ -0,0 +1,8 @@
+public class Age02 {
+	public static void main(String[] arg) {
+		int age = 20; //変数ageを20で初期化
+		System.out.println("私の年齢は" + age + "歳です");
+		age = 31; //変数ageに再度代入
+		System.out.println("私の年齢は" + age + "歳です");
+	}
+}
