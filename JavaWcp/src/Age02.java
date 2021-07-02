49e5f913b4dea1f74ea88f12c9917715db8e389f
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-03 07:50:22 +0900

    Add Pi01.java

--- JavaWcp/src/Age02.java
+++ JavaWcp/src/Age02.java
@@ -3,6 +3,9 @@ public class Age02 {
 		int age = 20; //変数ageを20で初期化
 		System.out.println("私の年齢は" + age + "歳です");
 		age = 31; //変数ageに再度代入
-		System.out.println("私の年齢は" + age + "歳です");
+		System.out.println("いや、本当の年齢は" + age + "歳です");
+		/*すでに値が入っている変数に代入すると
+		 *古い値は消滅し、新しい値に内容が書き換わる
+		 */
 	}
 }
---
+++ JavaWcp/src/Pi01.java
@@ -0,0 +1,12 @@
+public class Pi01 {
+	public static void main(String[] args) {
+		final double PI = 3.14; //定数として円周率を宣言
+		int pie = 5;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+		System.out.println("パイの半径を倍にします");
+		pie = 10;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+	}
+}
