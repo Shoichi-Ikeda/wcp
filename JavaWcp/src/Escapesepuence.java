f2640beaf9fa8c7522f901ec909ccffe17c75de0
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-04 17:14:01 +0900

    Add Escapesepuence.java

---
+++ JavaWcp/src/Escapesepuence.java
@@ -0,0 +1,6 @@
+public class Escapesepuence {
+	public static void main(String[] args) {
+		// System.out.println("私の好きな記号は二重引用符(")です");" //コンパイルエラー
+		System.out.println("私の好きな記号は二重引用符(\")です");
+	}
+}
