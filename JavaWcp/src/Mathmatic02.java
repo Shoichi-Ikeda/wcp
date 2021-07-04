25f5af108ba36d2b70583460d51c006f37417e8a
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-04 17:33:52 +0900

    Fix Mathmatic02.java

--- JavaWcp/src/Mathmatic02.java
+++ JavaWcp/src/Mathmatic02.java
@@ -14,16 +14,21 @@ public class Mathmatic02 {
 		int b;
 		int c;
 		b = 5;
-		//　b　をインクリメントした後に　a　が　c　に代入される
+		//　b　をインクリメントした後に　b　が　c　に代入される
 		c = ++b;
 		// それぞれの値は　b　が「6」で　c　も「6」
 		System.out.println(b);
 		System.out.println(c);
-		//　b　を　c　に代入した後に　a　がインクリメントされる
+		//　b　を　c　に代入した後に　b　がインクリメントされる
 		b = 5;
 		c = b++;
 		// それぞれの値は　b　が「6」で　c　が「5」
 		System.out.println(b);
 		System.out.println(c);
+		
+		int d = 10;
+		int e = 10;
+		System.out.println(++d + 50);
+		System.out.println(e++ + 50);
 	}
 }
