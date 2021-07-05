4512c97bdff4eb4b480635357bc5f4f9a0296bc2
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-05 15:29:52 +0900

    Add Print01.java

--- JavaWcp/src/Difference.java
+++ JavaWcp/src/Difference.java
@@ -1,7 +1,7 @@
 public class Difference {
 	public static void main(String[] args) {
-		double d = 8.5 / 2;
-		long l = 5 + 2L;
+		double d = 8.5 / 2; //2(int型)を2.0(double型)に変換
+		long l = 5 + 2L; //5(int型)を5L(long型)に変換
 		System.out.println(d);
 		System.out.println(l);
 	}
---
+++ JavaWcp/src/Print01.java
@@ -0,0 +1,8 @@
+public class Print01 {
+	public static void main(String[] args) {
+		String name = "池田";
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です。");
+	}
+}
