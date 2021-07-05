3104f6478f51676b1894c3981b2ecbc910609473
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-05 15:50:54 +0900

    Add Random.java

--- JavaWcp/src/IntegerParseInt.java
+++ JavaWcp/src/IntegerParseInt.java
@@ -1,4 +1,4 @@
-//文字列を数値に変換する
+//文字列(String型)を数値(int型)に変換する命令
 public class IntegerParseInt {
 	public static void main(String[] args) {
 		String age = "31";
--- JavaWcp/src/Random.java
+++ JavaWcp/src/Random.java
@@ -1,4 +1,7 @@
-
+//ランダムな数を生成する命令
 public class Random {
-
+	public static void main(String[] args) {
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたは多分" + r + "歳ですね。");
+	}
 }
