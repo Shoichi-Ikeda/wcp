f6a2251e233c7159d58d550aace55e6ba91e935e
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-06 10:38:02 +0900

    Add If03.java

---
+++ JavaWcp/src/Doorclose.java
@@ -0,0 +1,10 @@
+public class Doorclose {
+	public static void main(String[] args) {
+		// boolean doorClose = true;
+		boolean doorClose = false;
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		}
+	}
+}
---
+++ JavaWcp/src/If03.java
@@ -0,0 +1,13 @@
+//天気による行動の変化をJavaで表す
+public class If03 {
+	public static void main(String[] args) {
+		//boolean tenki = true;
+		boolean tenki = false;
+		if (tenki ==true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを観ます");
+		}
+	}
+}
