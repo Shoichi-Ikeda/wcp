a192e8b29a7d0cfccf96374bd0698ade7ce7688d
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-06 12:54:34 +0900

    Add Numbergame.java

--- JavaWcp/src/Hungry.java
+++ JavaWcp/src/Hungry.java
@@ -3,12 +3,16 @@ public class Hungry {
 		int isHungry = 1;
 		String food = "Pizza";
 		System.out.println("こんにちは！");
+		/*
 		if (isHungry == 0) {
 			System.out.println("お腹がいっぱいです。");
 		}
 		else {
 			System.out.println("はらぺこです。");
 		}
+		*/
+		//書き換えると↓↓↓
+		System.out.println(isHungry == 0 ? "お腹がいっぱいです。" : "はらぺこです。");
 		if (isHungry == 1) {
 			System.out.println(food + "をいただきます。");
 		}
