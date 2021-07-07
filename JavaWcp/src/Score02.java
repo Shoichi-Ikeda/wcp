1c631b63255a8d6bee81d69f48758b824c47c644
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-07 12:57:39 +0900

    Add Score03.java

--- JavaWcp/src/Score02.java
+++ JavaWcp/src/Score02.java
@@ -1,7 +1,9 @@
+// 配列の要素に値を代入
 public class Score02 {
 	public static void main(String[] args) {
 		int[] scores = new int[5];
 		scores[1] = 10; // 2つ目の要素 scores[1] に代入
 		System.out.println(scores[1]); // 2つ目の要素 score[1] の中身を表示
+		System.out.println(scores[2]); // 配列の要素は自動的に「0」で初期化
 	}
 }
---
+++ JavaWcp/src/Score03.java
@@ -0,0 +1,9 @@
+// ループによる全要素の利用
+public class Score03 {
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 60,};
+		for (int i = 0; i < scores.length; i++) {
+			System.out.println(scores[i]);
+		}
+	}
+}
