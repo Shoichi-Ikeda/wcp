d47ee2b2e4c22c2bee7581776c7609f4923b3c74
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-07 12:39:03 +0900

    Add Score02.java

---
+++ JavaWcp/src/Score02.java
@@ -0,0 +1,7 @@
+public class Score02 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		scores[1] = 10; // 2つ目の要素 scores[1] に代入
+		System.out.println(scores[1]); // 2つ目の要素 score[1] の中身を表示
+	}
+}
