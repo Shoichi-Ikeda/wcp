55a5432a3ea392f1577f3b6afc5455c61888851b
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-07 13:47:04 +0900

    Add Score03.java

--- JavaWcp/src/Score03_1.java
+++ JavaWcp/src/Score03_1.java
@@ -1,5 +1,5 @@
 // ループによる全要素の利用
-public class Score03 {
+public class Score03_1 {
 	public static void main(String[] args) {
 		int[] scores = {20, 30, 40, 50, 60,};
 		for (int i = 0; i < scores.length; i++) {
---
+++ JavaWcp/src/Score03_2.java
@@ -0,0 +1,9 @@
+// 拡張for文
+public class Score03_2 {
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 60,};
+		for (int value : scores) {
+			System.out.println(value);
+		}
+	}
+}
