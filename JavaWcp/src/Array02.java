f61522d869da2a8b9798b881bc071336ce8e49b7
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-08 11:52:30 +0900

    Add Array02.java

---
+++ JavaWcp/src/Array02.java
@@ -0,0 +1,8 @@
+// 親配列と小配の要素数を表示
+public class Array02 {
+	public static void main(String[] args) {
+		int[][] scores = {{40, 50, 60}, {80, 70, 60}};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+	}
+}
