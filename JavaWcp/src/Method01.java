4f28d54d0abd00cf32feb30bf2427abef50981ac
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-10 08:42:55 +0900

    Add Method01.java

---
+++ JavaWcp/src/Method01.java
@@ -0,0 +1,10 @@
+public class Method01 {
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します。");
+		hello(); // helloメソッドを呼び出す
+		System.out.println("メソッドの呼び出しが終わりました。");		
+	}
+	public static void hello() {
+		System.out.println("優海さん、こんにちは");
+	}
+}
