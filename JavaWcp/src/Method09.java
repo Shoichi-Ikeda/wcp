67fa5db7a14bc4afb19e6acf06ea61db129effe0
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-11 16:07:59 +0900

    Add Method09.java

---
+++ JavaWcp/src/Method09.java
@@ -0,0 +1,15 @@
+public class Method09 {
+	public static void introduceOneself() {
+		String name = "池田翔一";
+		int age = 31;
+		double tall = 173.2;
+		char eto = '巳';
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println(eto + "年生まれの" + age + "で、");
+		System.out.println("身長は" + tall + "です。");
+	}
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+}
