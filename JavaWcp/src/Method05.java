70aa4faaf7ecea7562e51f6510038bad559f49d5
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-11 09:20:52 +0900

    Add Method05.java

---
+++ JavaWcp/src/Method05.java
@@ -0,0 +1,11 @@
+// 戻り値の例
+public class Method05 {
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
+	}
+	public static void main(String[] args) {
+		int ans = add(100, 10); // addメソッドの呼び出し(110に化ける)
+		System.out.println("100 + 10 = " + ans);
+	}
+}
