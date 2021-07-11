812db2126413b36ae1fd46aebf80db672601c587
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-11 09:33:36 +0900

    Add Method06.java

---
+++ JavaWcp/src/Method06.java
@@ -0,0 +1,10 @@
+// 戻り値をそのまま使う
+public class Method06 {
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
+	}
+	public static void main(String[] args) {
+		System.out.println(add(add(10, 20), add(30, 40))); // add(30, 70)に化ける
+	}
+}
