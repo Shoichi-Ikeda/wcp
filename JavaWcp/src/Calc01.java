2bd393ae4148d5ce6ab0617848b4ee1c352ed24f
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-12 11:44:18 +0900

    Add Calc01.java

---
+++ JavaWcp/src/Calc01.java
@@ -0,0 +1,9 @@
+public class Calc01 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		int total = CalcLogic01.tasu(a,b);
+		int delta = CalcLogic01.hiku(a,b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+}
