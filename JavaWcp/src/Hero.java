8f6b8c5e35585e7af2ed123b1abbe80211bec45f
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-15 21:15:50 +0900

    Add Hero.java

--- JavaWcp/src/Hero.java
+++ JavaWcp/src/Hero.java
@@ -1,21 +1,28 @@
 public class Hero {
 	//属性の定義
-	String name;
-	int hp;
+	String name; //名前の変数宣言
+	int hp; //HPの変数宣言
 	//操作の定義
 	public void attack() {
 		
 	}
 	public void sleep() {
-		
+		this.hp = 100; //自分自身のhpフィールド
+		System.out.println(this.name + "は、眠って回復した！");
 	}
-	public void sit(int sec) {
-		
+	public void sit(int sec) { //何秒座るか引数で受け取る
+		this.hp += hp; //座る秒数だけHPを増やす
+		System.out.println(this.name + "は、" + sec + "秒座った！");
+		System.out.println("HPが" + sec + "ポイント回復した！");
 	}
 	public void slip() {
-		
+		this.hp -= 5;
+		System.out.println(this.name + "は、転んだ！");
+		System.out.println("5のダメージ！");
 	}
 	public void run() {
-		
+		System.out.println(this.name + "は、逃げ出した！");
+		System.out.println("GAMEOVER");
+		System.out.println("最終HPは" + this.hp + "でした。");
 	}
 }
