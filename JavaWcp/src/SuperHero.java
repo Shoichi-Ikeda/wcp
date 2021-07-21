bcc80327996d0cf9c57252a4cec7518f52a65eeb
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-21 14:43:11 +0900

    Fix SuperHero.java

--- JavaWcp/src/SuperHero.java
+++ JavaWcp/src/SuperHero.java
@@ -9,4 +9,8 @@ public class SuperHero extends Hero {
 		this.flying = false;
 		System.out.println("着地した！");
 	}
+	//SuperHeroクラスにrun()を再定義する
+	public void run() { //親クラスにも定義してあるが、小クラスで再定義するメソッド
+		System.out.println(this.name + "は撤退した！");
+	}
 }
