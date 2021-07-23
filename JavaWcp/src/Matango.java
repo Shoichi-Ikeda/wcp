737337a49329e72fbfe8701533245a1dc9700121
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-23 09:45:10 +0900

    Fix Marango.java

--- JavaWcp/src/Matango.java
+++ JavaWcp/src/Matango.java
@@ -1,7 +1,15 @@
 public class Matango {
-	int hp; //HPの変数宣言
+	int hp = 50; //HPの変数宣言
 	final int LEVEL = 10; //フィールドを定数として宣言し、初期値も設定
 	char suffix;
+	public Matango(char suffix) {
+		this.suffix = suffix;
+	}
+	public void attack(Hero h) {
+		System.out.println("キノコ" + this.suffix + "の攻撃！");
+		System.out.println("10のダメージ");
+		h.hp -= 10;
+	}
 	public void run() {
 		System.out.println("おばけキノコ" + this.suffix + "は、逃げ出した！");
 	}
