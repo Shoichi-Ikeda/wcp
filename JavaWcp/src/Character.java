d868220e142213bd9f79dfb5800f7a2d1c6bb06f
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-24 08:13:47 +0900

    Fix Character.java

--- JavaWcp/src/Character.java
+++ JavaWcp/src/Character.java
@@ -1,4 +1,11 @@
-
-public class Character {
-
+//抽象クラスとしてcharacterを宣言→newによるインスタンス化の禁止
+public abstract class Character {
+	String name;
+	int hp;
+	//逃げる
+	public void run() {
+		System.out.println(this.name + "は、逃げ出した！");
+	}
+	//戦うの抽象メソッドを宣言   
+	public abstract void attack(Matango m);
 }
