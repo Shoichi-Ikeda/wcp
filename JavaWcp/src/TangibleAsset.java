5efd947ada6465dd63e40d1ec0f3c391a4282a28
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-26 18:19:20 +0900

    Add TangibleAsset.java

---
+++ JavaWcp/src/TangibleAsset.java
@@ -0,0 +1,20 @@
+public class TangibleAsset {
+	String name;
+	int price;
+	String color;
+	
+	public TangibleAsset(String name, int price, String color) {
+		this.name;
+		this.price;
+		this.color;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getPrice() {
+		return this.price;
+	}
+	public String getColor() {
+		return this.color;
+	}
+}
