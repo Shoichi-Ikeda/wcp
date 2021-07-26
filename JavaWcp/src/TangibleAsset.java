bacefac27d80f4721329eb4bf30b1f93908452cb
Shoichi-Ikeda <shoichann@gmail.com>
2021-07-26 18:24:07 +0900

    Book.java

--- JavaWcp/src/TangibleAsset.java
+++ JavaWcp/src/TangibleAsset.java
@@ -4,9 +4,9 @@ public class TangibleAsset {
 	String color;
 	
 	public TangibleAsset(String name, int price, String color) {
-		this.name;
-		this.price;
-		this.color;
+		this.name = name;
+		this.price = price;
+		this.color = color;
 	}
 	public String getName() {
 		return this.name;
