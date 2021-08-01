3872647ea0878feed828da1113713a644f43e0b6
Shoichi-Ikeda <shoichann@gmail.com>
2021-08-01 10:43:12 +0900

    Fix Hero.java

--- JavaWcp/src/Hero.java
+++ JavaWcp/src/Hero.java
@@ -43,15 +43,13 @@ public class Hero extends Character {
 		System.out.println(this.name + "は、逃げ出した！");
 		System.out.println("GAMEOVER");
 	}
-	/*
 	//newされた直後に自動に実行される処理を書いたメソッド(コンストラクタ①)
 	public Hero(String name) { //引数として文字列を1つ受け取る
 		this.hp = 100;
 		this.name = name; //引数の値でnameフィールドを初期化
 	}
-	*/
 	//コンストラクタ②
 	public Hero() { //新しく作ったコンストラクタ
-		// this("ダミー"); //コンストラクタ①を呼び出すようJVMに依頼する
+		this("ダミー"); //コンストラクタ①を呼び出すようJVMに依頼する
 	}
 }
