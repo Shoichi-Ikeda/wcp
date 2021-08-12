59c6b6ff8f42b6183fe8c72a7e058821418bdb32
Shoichi-Ikeda <shoichann@gmail.com>
2021-08-12 21:48:36 +0900

    Add Main04.java

--- JavaWcp/src/Main04.java
+++ JavaWcp/src/Main04.java
@@ -1,10 +1,11 @@
 //StringBilderを利用し文字列を1万回連結
-public class StringBilder {
+public class Main04 {
 	public static void main(String[] args) {
-		StringBilder sb = new StringBilder();
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 10000; i++) {
 			sb.append("Java");
 		}
 		String s = sb.toString();
+		System.out.println(s);
 	}
 }
