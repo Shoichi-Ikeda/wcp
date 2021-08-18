f434f3ca87c7e4bc4a1cad9971133fdb174b6ab5
Shoichi-Ikeda <shoichann@gmail.com>
2021-08-18 11:38:45 +0900

    Add Main05_02.java

--- JavaWcp/src/Main05_2.java
+++ JavaWcp/src/Main05_2.java
@@ -6,6 +6,7 @@ public class Main05_2 {
 		LocalDate now = LocalDate.now();
 		LocalDate future = now.plusDays(100);
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+		System.out.println(now);
 		System.out.println(future.format(fmt));
 	}
 }
