9b43f6301da425ed292d5cf2f7de5ec8f914e384
Shoichi-Ikeda <shoichann@gmail.com>
2021-08-19 21:22:34 +0900

    Fix Main05_02.java

--- JavaWcp/src/Main05_2.java
+++ JavaWcp/src/Main05_2.java
@@ -4,9 +4,11 @@ import java.time.format.DateTimeFormatter;
 public class Main05_2 {
 	public static void main(String[] args) {
 		LocalDate now = LocalDate.now();
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
 		LocalDate future = now.plusDays(100);
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
 		System.out.println(now);
-		System.out.println(future.format(fmt));
+		System.out.println(now.format(fmt1));
+		System.out.println(future.format(fmt2));
 	}
 }
