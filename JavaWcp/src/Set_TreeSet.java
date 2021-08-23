cb9ae5f7099c8011ce39ab5ae66bf6725e3265b6
Shoichi-Ikeda <shoichann@gmail.com>
2021-08-23 21:07:48 +0900

    Fix Set_TreeSet.java

--- JavaWcp/src/Set_TreeSet.java
+++ JavaWcp/src/Set_TreeSet.java
@@ -6,13 +6,15 @@ public class Set_TreeSet {
 	public static void main(String[] args) {
 		Set<String> words = new TreeSet<String>();
 		words.add("Dog");
+		words.add("Bird");
 		words.add("Cat");
 		words.add("Fox");
 		words.add("Wolf");
 		words.add("Panda");
+		words.add("Animal");
 	
 		for (String s : words) {
-			System.out.print(s + "→");
+			System.out.print(s + "→"); //自然順序付け(String型は辞書順)で整列
 		}
 	}
 }
