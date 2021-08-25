f29ead04cda406d4160a11725a731f10c3b20db4
Shoichi-Ikeda <shoichann@gmail.com>
2021-08-25 20:52:09 +0900

    Fix Exception.java

--- JavaWcp/src/Exception.java
+++ JavaWcp/src/Exception.java
@@ -1,9 +1,13 @@
 // 例外処理を用意していないと…
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Exception {
 	public static void main(String[] args) {
-		FileWriter fw = new FileWriter("data.txt");
-		
+		try {
+			FileWriter fw = new FileWriter("data.txt");
+		} catch (IOException e) {
+			System.out.println("エラーが発生しました。"); //例外的状況になあった時に備えて記述された代替処理
+		}
 	}
 }
